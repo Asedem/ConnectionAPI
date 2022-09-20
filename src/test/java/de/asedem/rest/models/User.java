a19e779064122aaea8e76e983e823ee4365d650e
Asedem <simon.oberzier@juniorjob.de>
2022-09-20 20:45:52 +0200

    Removed unused toString override

--- src/test/java/de/asedem/rest/models/User.java
+++ src/test/java/de/asedem/rest/models/User.java
@@ -3,10 +3,4 @@ package de.asedem.rest.models;
 public record User(
         String user_name,
         String uuid
-) {
-
-    @Override
-    public String toString() {
-        return "User: " + user_name + " (" + uuid + ")";
-    }
-}
+) { }
